13c76fa08f182128fc2debf5ec9aeaf50a2c9236
Žikica Drača <zikica.draca@gmail.com>
2023-07-04 01:12:38 +0200

    new enter header

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -574,9 +574,17 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"1SICI":[function(require,module,exports) {
+//Hamburger and Responsive navigation menu toggle
 const btnHam = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+const btnClose = document.querySelector(".close-btn");
 btnHam.addEventListener("click", ()=>{
     btnHam.classList.toggle("open");
+    nav.classList.toggle("active");
+});
+//navigation bar effects on scroll
+window.addEventListener("scroll", ()=>{
+    document.querySelector("header").classList.toggle("sticky", window.scrollY > 0);
 });
 
 },{}]},["gEwwu","1SICI"], "1SICI", "parcelRequire6975")
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,16 @@
+//Hamburger and Responsive navigation menu toggle
 const btnHam = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+const btnClose = document.querySelector('.close-btn');
 
 btnHam.addEventListener('click', () => {
   btnHam.classList.toggle('open');
+  nav.classList.toggle('active');
+});
+
+//navigation bar effects on scroll
+window.addEventListener('scroll', () => {
+  document
+    .querySelector('header')
+    .classList.toggle('sticky', window.scrollY > 0);
 });
