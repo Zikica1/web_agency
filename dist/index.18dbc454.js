d75e1c123dc3ab90029d01594420511efdaf08c9
Žikica Drača <zikica.draca@gmail.com>
2023-07-04 20:25:32 +0200

    header

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -577,10 +577,28 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 //Hamburger and Responsive navigation menu toggle
 const btnHam = document.querySelector(".hamburger");
 const nav = document.querySelector("nav");
-const btnClose = document.querySelector(".close-btn");
+const btnClose = document.querySelector(".btn-close");
 btnHam.addEventListener("click", ()=>{
-    btnHam.classList.toggle("open");
-    nav.classList.toggle("active");
+    btnHam.classList.add("open");
+    nav.classList.add("active");
+});
+btnClose.addEventListener("click", ()=>{
+    nav.classList.remove("active");
+    btnHam.classList.remove("open");
+});
+//active link
+const links = document.querySelectorAll(".link");
+let activeLink = 0;
+links.forEach((link, i)=>{
+    link.addEventListener("click", ()=>{
+        if (activeLink != i) {
+            links[activeLink].classList.remove("active");
+            link.classList.add("active");
+            activeLink = i;
+        }
+        nav.classList.remove("active");
+        btnHam.classList.remove("open");
+    });
 });
 //navigation bar effects on scroll
 window.addEventListener("scroll", ()=>{
