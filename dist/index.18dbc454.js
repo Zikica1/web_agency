793db4577aa2f71d80d789bc44491bc1ea41e0eb
Žikica Drača <zikica.draca@gmail.com>
2023-07-17 18:32:46 +0200

    entering services

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -604,6 +604,56 @@ links.forEach((link, i)=>{
 window.addEventListener("scroll", ()=>{
     document.querySelector("header").classList.toggle("sticky", window.scrollY > 0);
 });
+//Observer - hero
+const heroItems = document.querySelectorAll(".load-item");
+const observerHero = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        entry.target.classList.toggle("animate", entry.isIntersecting);
+        if (entry.isIntersecting) observerHero.unobserve(entry.target);
+    });
+}, {
+    rootMargin: "0px 0px -110px 0px"
+});
+heroItems.forEach((item)=>{
+    observerHero.observe(item);
+});
+/* Slider */ const contents = document.querySelectorAll(".content");
+const buttons = document.querySelectorAll(".nav-btn");
+const sliderNav = function(i) {
+    contents.forEach((content)=>{
+        content.classList.remove("active");
+    });
+    buttons.forEach((button)=>{
+        button.classList.remove("active");
+    });
+    contents[i].classList.add("active");
+    buttons[i].classList.add("active");
+};
+buttons.forEach((btn, i)=>{
+    btn.addEventListener("click", ()=>{
+        sliderNav(i);
+    });
+});
+/* Modal services */ const btnLearn = document.querySelectorAll(".learn-more");
+const servicesMod = document.querySelectorAll(".service-modal");
+const modalClose = document.querySelectorAll(".modal-close-btn");
+const portfolioService = function(i) {
+    servicesMod[i].classList.add("active");
+    document.body.style.overflow = "hidden";
+};
+btnLearn.forEach((learn, i)=>{
+    learn.addEventListener("click", ()=>{
+        portfolioService(i);
+    });
+});
+modalClose.forEach((btn)=>{
+    btn.addEventListener("click", ()=>{
+        servicesMod.forEach((modal)=>{
+            modal.classList.remove("active");
+            document.body.style.overflow = "visible";
+        });
+    });
+});
 
 },{}]},["gEwwu","1SICI"], "1SICI", "parcelRequire6975")
 
