a61fc918aa38bf16ff0f71c963c51666f7874726
Žikica Drača <zikica.draca@gmail.com>
2023-07-21 18:43:47 +0200

    new entering statistics

--- src/js/main.js
+++ src/js/main.js
@@ -138,3 +138,50 @@ modalClose.forEach((btn) => {
     });
   });
 });
+
+// Counter
+const counters = document.querySelectorAll('.counter');
+let scrollStarted = false;
+
+function scrollPage() {
+  const scrollPos = window.scrollY;
+  console.log(scrollPos);
+
+  if (scrollPos > 3103 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPos < 3103 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  }
+}
+
+document.addEventListener('scroll', scrollPage);
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute('data-target');
+
+      const c = +counter.innerText;
+
+      const increment = target / 30;
+      if (c < target) {
+        counter.innerText = `${Math.ceil(c + increment)}`;
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+function reset() {
+  counters.forEach((counter) => {
+    counter.innerHTML = '0';
+  });
+}
