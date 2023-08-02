f59700b272fa55fcfd5f19bc339a4c1da519fd80
Žikica Drača <zikica.draca@gmail.com>
2023-08-02 19:29:33 +0200

    enter card section about

--- src/js/about.js
+++ src/js/about.js
@@ -7,15 +7,35 @@ load();
 
 //about-observer
 const aboutImg = document.querySelector('.about-right-col .img-right-col');
+const aboutDiv = document.querySelector('.about-left-col .about-divider');
+const aboutTex = document.querySelector('.about-text');
+const aboutBtn = document.querySelector('.btn-about-pri');
+const visionTitle = document.querySelector('.vision-title');
+const visionDiv = document.querySelector('.vision-divider');
+const visionCard = document.querySelectorAll('.our-vision-card');
 
-const observerAbout = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    entry.target.classList.toggle('animation', entry.isIntersecting);
-    if (entry.isIntersecting) observerAbout.unobserve(entry.target);
-  });
-});
+const observerAbout = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle('animation', entry.isIntersecting);
+      if (entry.isIntersecting) observerAbout.unobserve(entry.target);
+    });
+  },
+  {
+    rootMargin: '0px 0px -50px 0px',
+  }
+);
 
 observerAbout.observe(aboutImg);
+observerAbout.observe(aboutDiv);
+observerAbout.observe(aboutTex);
+observerAbout.observe(aboutBtn);
+observerAbout.observe(visionTitle);
+observerAbout.observe(visionDiv);
+
+visionCard.forEach((card) => {
+  observerAbout.observe(card);
+});
 
 //Typewriter
 let i = 0;
